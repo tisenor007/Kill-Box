989e7ba70b19112acb2bd2f978eaf244e34c441c
tisenor007 <ronesinotnert@gmail.com>
2021-01-14 15:10:48 -0400

    Initial commit

---
+++ Assets/KillTrigger.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class KillTrigger : MonoBehaviour
+{
+   
+        
+        void OnTriggerEnter()
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
